77378f0e67f034123497a0bffb8a88f0ba8dd0c0
Diego <godie8a@hotmail.com>
2022-09-26 21:14:57 -0500

    Fetch data and display missions

--- src/redux/Missions/index.js
+++ src/redux/Missions/index.js
@@ -1,20 +1,26 @@
 // action types
-export const TEST = 'TEST';
+// const JOIN_MISSIONS = 'JOIN_MISSIONS';
+// const LEAVE_MISSIONS = 'LEAVE_MISSIONS';
+const UPDATE_MISSIONS = 'UPDATE_MISSIONS';
 
 // state
 const initialState = [];
 
 // action creators
-export const test = () => ({
-  type: TEST,
-});
+export const showMissions = () => async (dispatch) => {
+  const res = await fetch('https://api.spacexdata.com/v3/missions');
+  const data = await res.json();
+  dispatch({
+    type: UPDATE_MISSIONS,
+    data,
+  });
+};
 
 // reducer
 export default function missionReducer(state = initialState, action) {
-  const { type } = action;
-  switch (type) {
-    case TEST:
-      return state;
+  switch (action.type) {
+    case UPDATE_MISSIONS:
+      return action.data;
     default:
       return state;
   }
